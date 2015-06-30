5fea03cf50db0ae9ff8da196bf69d5297fe75b9f
DaniKog <dkdko65482@gmail.com>
2015-06-30 00:03:53 -0400

    Floating Table Header
    Added JQuery to make the Table float

---
+++ ScrollingScript.js
@@ -0,0 +1,15 @@
+var $j = jQuery;
+jQuery(function ($) {
+ 
+$(document).ready(function(){
+jQuery(document).ready(function($) {
+   $('table.audio').floatThead({
+	useAbsolutePositioning: false
+	// absolutePositioning is better for
+	// highly dynamic sites
+	// (which this is not)
+});
+});
+ 
+});
+});
